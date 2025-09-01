184162d8758a3e72f66569cb7828d6a391fa809f
Kaleu096 <almeida.kaleu@escola.pr.gov.br>
2025-09-01 08:30:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -87,8 +87,8 @@ function mostraPergunta() {
     mostraAlternativas();
 }
 
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas) {
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
